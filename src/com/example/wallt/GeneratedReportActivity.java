2102ed4291a3b34006e8d587aeba603816470405
Thomas Harris <teharris07@gmail.com>
2014-03-12 10:50:03 -0400

    updated reportfragment

---
+++ src/com/example/wallt/GeneratedReportActivity.java
@@ -0,0 +1,22 @@
+package com.example.wallt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GeneratedReportActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_generated_report);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.generated_report, menu);
+		return true;
+	}
+
+}
--- src/com/example/wallt/ReportsFragment.java
+++ src/com/example/wallt/ReportsFragment.java
@@ -9,21 +9,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class ReportsFragment extends Fragment {
 	
-	private int[] checklistValues = new int[2];
+	private int[] checklistValues = new int[4];
 	
 	CheckBox spendingCheckBox;
 	CheckBox incomeCheckBox;
-	CheckBox budgetCheckBox;
-	
+	CheckBox cashflowCheckBox;
+	CheckBox accountlistingCheckBox;
+	DatePicker fromDate;
+	DatePicker toDate;
 	Button reportButton;
-	
-	EditText dateFrom;
-	EditText dateTo;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,12 +31,13 @@ public class ReportsFragment extends Fragment {
 		View windows = inflater.inflate(R.layout.fragment_reports, container, false);
 		spendingCheckBox = (CheckBox) windows.findViewById(R.id.checkBoxSpending);
 		incomeCheckBox = (CheckBox) windows.findViewById(R.id.checkBoxIncome);
-		budgetCheckBox = (CheckBox) windows.findViewById(R.id.checkBoxBudget);
+		cashflowCheckBox = (CheckBox) windows.findViewById(R.id.checkBoxCashFlow);
+		accountlistingCheckBox = (CheckBox) windows.findViewById(R.id.checkAccountsListing);
 		
 		setUpIntroCheckBoxes();
 		
-		dateFrom = (EditText) windows.findViewById(R.id.editTextDate1);
-		dateTo = (EditText) windows.findViewById(R.id.editTextDate2);
+		fromDate = (DatePicker) windows.findViewById(R.id.Datefrom);
+		toDate = (DatePicker) windows.findViewById(R.id.Dateto);
 		
 		reportButton = (Button) windows.findViewById(R.id.buttonReport);
 		setButtonOnClickListener();
@@ -60,6 +61,11 @@ public class ReportsFragment extends Fragment {
 					boolean isChecked) {
 				// TODO Auto-generated method stub
 				checklistValues[0] = (spendingCheckBox.isChecked()) ? 1:0;
+				if (spendingCheckBox.isChecked()) {
+					cashflowCheckBox.setChecked(false);
+					accountlistingCheckBox.setChecked(false);
+					incomeCheckBox.setChecked(false);
+				}
 			}
 			
 		});
@@ -71,21 +77,46 @@ public class ReportsFragment extends Fragment {
 					boolean isChecked) {
 				// TODO Auto-generated method stub
 				checklistValues[1] = (incomeCheckBox.isChecked()) ? 1:0;
+				if (incomeCheckBox.isChecked()) {
+					spendingCheckBox.setChecked(false);
+					cashflowCheckBox.setChecked(false);
+					accountlistingCheckBox.setChecked(false);
+				}
 			}
 			
 		});
 		
-		budgetCheckBox.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
+		cashflowCheckBox.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
 
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView,
 					boolean isChecked) {
 				// TODO Auto-generated method stub
-				checklistValues[1] = (budgetCheckBox.isChecked()) ? 1:0;
+				checklistValues[2] = (cashflowCheckBox.isChecked()) ? 1:0;
+				if (cashflowCheckBox.isChecked()) {
+					spendingCheckBox.setChecked(false);
+					accountlistingCheckBox.setChecked(false);
+					incomeCheckBox.setChecked(false);
+				}
 			}
 			
 		});
-		// TODO Auto-generated method stub
+		
+		accountlistingCheckBox.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				// TODO Auto-generated method stub
+				checklistValues[3] = (accountlistingCheckBox.isChecked()) ? 1:0;
+				if (accountlistingCheckBox.isChecked()) {
+					spendingCheckBox.setChecked(false);
+					cashflowCheckBox.setChecked(false);
+					incomeCheckBox.setChecked(false);
+				}
+			}
+			
+		});
 		
 	}
 }
